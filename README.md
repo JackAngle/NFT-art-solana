f916ff8a5bb61bc0948628825b7f4394e2c52826
JackAngle156 <lehuythinh156@gmail.com>
2021-10-24 18:06:27 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Auto-generated NFT art on solana blockchain
+
+## Hashlip Art engine to create multiple different instances of artworks based on provided layers.
+
+## Metaplex framework for decentralized NFT.
+
+## CandyMachine on-chain Solana program (or smart contract) for managing fair mint.
+
+
+## Hashlip's tutorial: https://www.youtube.com/watch?v=35RO0lAEIxE
---
+++ hashlips_art_engine-main/src/config.js
@@ -0,0 +1,107 @@
+"use strict";
+
+const path = require("path");
+const isLocal = typeof process.pkg === "undefined";
+const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const { MODE } = require(path.join(basePath, "constants/blend_mode.js"));
+const { NETWORK } = require(path.join(basePath, "constants/network.js"));
+
+const network = NETWORK.sol;
+
+// General metadata for Ethereum
+const namePrefix = "Unoriginal original";
+const description = "A original project with a lot of unoriginal";
+const baseUri = "ipfs://NewUriToReplace";
+
+const solanaMetadata = {
+  symbol: "UO",
+  seller_fee_basis_points: 500, // Define how much % you want from secondary market sales 1000 = 10%
+  external_url: "https://github.com/JackAngle",
+  creators: [
+    {
+      address: "C3pXGrbaPrjvdjWkatFV88CUhkmc1BMe4PmnBkq9YXxh",
+      share: 100,
+    },
+  ],
+};
+
+// If you have selected Solana then the collection starts from 0 automatically
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 56,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Eyeball" },
+      { name: "Eye color" },
+      { name: "Iris" },
+      { name: "Shine" },
+      { name: "Bottom lid" },
+      { name: "Top lid" },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 512,
+  height: 512,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.width / format.height,
+  imageName: "preview.png",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  solanaMetadata,
+};
